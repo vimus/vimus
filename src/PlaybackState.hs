128ead781d622401e2c1fdb92b64023a973d5cc7
Simon Hengel <sol@typeful.net>
2012-02-25 09:31:18 +0100

    Some code simplification

--- src/PlaybackState.hs
+++ src/PlaybackState.hs
@@ -3,7 +3,6 @@ module PlaybackState(onChange, PlaybackState, playState, playStatus, elapsedTime
 import Data.Foldable (for_)
 
 import Control.Monad
-import Control.Concurrent (forkIO, MVar, newEmptyMVar, putMVar, takeMVar)
 import Control.Monad.Trans (liftIO, MonadIO)
 import qualified Network.MPD as MPD hiding (withMPD)
 import Network.MPD (MPD(), Seconds)
@@ -23,30 +22,20 @@ data PlaybackState = PlaybackState {
 elapsedTime :: PlaybackState -> (Seconds, Seconds)
 elapsedTime s = case elapsedTime_ s of (c, t) -> (round c, t)
 
--- |
--- Execute action on each change of the playback status.
+-- | Execute action on each change of the playback state.
 onChange :: (PlaybackState -> IO ()) -> MPD ()
-onChange action = do
-  var <- liftIO newEmptyMVar
-
-  -- execute action on each change of var (in a separate thread)
-  _ <- liftIO . forkIO $ forever $ do
-    state <- takeMVar var
-    action state
-
-  -- wait for changes and put them into var
-  forever $ do
-    timer <- queryState var
-    _ <- iterateUntil (\xs -> MPD.PlayerS `elem` xs || MPD.OptionsS `elem` xs) MPD.idle
-    for_ timer Timer.stop
+onChange action = forever $ do
+  timer <- queryState action
+  _ <- iterateUntil (\xs -> MPD.PlayerS `elem` xs || MPD.OptionsS `elem` xs) MPD.idle
+  for_ timer Timer.stop
 
 -- |
--- Query the playback state and put the result into given MVar.  If a song is
--- currently being played, start a timer, that repeatedly updates the elapsed
--- time and puts the result into given MVar.  The timer, if any, is returned
--- and the caller is responsible for stopping it.
-queryState :: MVar PlaybackState -> MPD (Maybe Timer)
-queryState var = do
+-- Query the playback state and call given action.  If a song is currently
+-- being played, start a timer, that repeatedly updates the elapsed time and
+-- calls given action.  The timer, if any, is returned and the caller is
+-- responsible for stopping it.
+queryState :: (PlaybackState -> IO ()) -> MPD (Maybe Timer)
+queryState action = do
 
   -- query state
   status <- MPD.status
@@ -54,13 +43,13 @@ queryState var = do
 
   -- put state into var
   let state = PlaybackState (MPD.stState status) status (MPD.stTime status) song
-  liftIO $ putMVar var state
+  liftIO $ action state
 
   -- start timer, if playing
   if playState state == MPD.Playing
     then do
       timer <- Timer.start 1000000 $ \count -> do
-        putMVar var $ updateElapsedTime state count
+        action (updateElapsedTime state count)
       return $ Just timer
     else
       return Nothing
