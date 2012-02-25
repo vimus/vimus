33e5856bc4d7614d7185348fb4aad8ad239dc0c0
Simon Hengel <sol@typeful.net>
2012-02-25 16:47:40 +0100

    PlaybackState: Only update timer when really needed

--- src/PlaybackState.hs
+++ src/PlaybackState.hs
@@ -4,8 +4,10 @@ import           Prelude hiding (mapM_)
 import           Data.Foldable (mapM_)
 import           Data.List (find)
 
-import           Control.Monad (when)
-import           Control.Monad.State (StateT, liftIO, evalStateT, gets, modify)
+import           Control.Monad (when, forever)
+import           Control.Monad.State (liftIO, evalStateT, gets, modify)
+
+import           Data.Default
 
 import qualified Network.MPD as MPD hiding (withMPD)
 import qualified Network.MPD.Commands.Extensions as MPDE
@@ -28,61 +30,59 @@ data State = State {
 , timer    :: Maybe Timer
 }
 
+instance Default State where
+  def = State def def
+
 elapsedTime :: PlaybackState -> (Seconds, Seconds)
 elapsedTime s = case elapsedTime_ s of (c, t) -> (round c, t)
 
--- | Execute action on each change of the playback state.
+-- | Execute given actions on changes.
 onChange :: ([Song] -> IO ()) -> (PlaybackState -> IO ()) -> MPD ()
-onChange plChanged action = do
-  MPDE.getPlaylist >>= liftIO . plChanged
-  pl <- MPDE.getPlaylist
-  liftIO (plChanged pl)
-  evalStateT go (State pl Nothing)
+onChange plChanged action =
+  evalStateT (updatePlaylist >> updateTimer >> idle) def
   where
 
-    go = do
-      gets playlist >>= queryState action
-
+    -- Wait for changes.
+    idle = forever $ do
       r <- MPD.idle [MPD.PlayerS, MPD.OptionsS, MPD.PlaylistS]
+      when (MPD.PlaylistS `elem` r)
+        updatePlaylist
+      when (MPD.PlayerS `elem` r || MPD.OptionsS `elem` r)
+        updateTimer
 
-      when (MPD.PlaylistS `elem` r) $ do
-        pl <- MPDE.getPlaylist
-        liftIO (plChanged pl)
-        modify (\st -> st {playlist = pl})
-      go
+    -- Fetch current playlist, update state, and call `plChanged` action.
+    updatePlaylist = do
+      pl <- MPDE.getPlaylist
+      modify (\st -> st {playlist = pl})
+      liftIO $ plChanged pl
+
+    -- Query the playback state and call `action`.  If a song is currently
+    -- being played, start a timer, that repeatedly updates the elapsed time
+    -- and calls `action`.
+    updateTimer = do
+
+      -- stop old timer (if any)
+      gets timer >>= mapM_ Timer.stop
+      modify (\st -> st {timer = Nothing})
+
+      -- query state and call action
+      status <- MPD.status
+      pl <- gets playlist
+      let song = maybe (const Nothing) findSongWithId (MPD.stSongID status) pl
+      let s = PlaybackState (MPD.stState status) status (MPD.stTime status) song
+      liftIO $ action s
+
+      -- start timer, if playing
+      when (playState s == MPD.Playing) $ do
+        t <- Timer.start 1000000 $ \count -> do
+          action (updateElapsedTime s count)
+        modify (\st -> st {timer = Just t})
 
 -- | Find first song with given id.
 findSongWithId :: Id -> [Song] -> Maybe Song
 findSongWithId songId = find ((== Just songId) . MPD.sgId)
 
--- |
--- Query the playback state and call given action.  If a song is currently
--- being played, start a timer, that repeatedly updates the elapsed time and
--- calls given action.  The timer, if any, is returned and the caller is
--- responsible for stopping it.
-queryState :: (PlaybackState -> IO ()) -> [Song] -> StateT State MPD ()
-queryState action pl = do
-
-  -- stop old timer
-  gets timer >>= mapM_ Timer.stop
-  modify (\st -> st {timer = Nothing})
-
-  -- query state
-  status <- MPD.status
-  let song = maybe (const Nothing) findSongWithId (MPD.stSongID status) pl
-
-  -- put state into var
-  let s = PlaybackState (MPD.stState status) status (MPD.stTime status) song
-  liftIO $ action s
-
-  -- start timer, if playing
-  when (playState s == MPD.Playing) $ do
-    t <- Timer.start 1000000 $ \count -> do
-      action (updateElapsedTime s count)
-    modify (\st -> st {timer = Just t})
-
--- |
--- Increase elapsed time of given playback state by given seconds.
+-- | Increase elapsed time of given playback state by given seconds.
 updateElapsedTime :: PlaybackState -> Double -> PlaybackState
 updateElapsedTime s seconds = s {elapsedTime_ = (timeElapsed + seconds, timeTotal)}
   where
