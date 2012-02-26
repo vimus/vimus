a92a22ad7bad1056160cd95f752f105916b2c8eb
Simon Hengel <sol@typeful.net>
2012-02-26 15:04:56 +0100

    Get rid of a race condition
    stopTimer did not block, so the timer action could have been run after
    the call to statusChanged in PlaybackState.

--- src/Timer.hs
+++ src/Timer.hs
@@ -8,24 +8,24 @@ import           Control.Concurrent
 import           Control.Monad.Trans (liftIO, MonadIO)
 import           Control.Monad (when)
 
-newtype Timer = Timer (MVar ())
+newtype Timer = Timer (MVar Bool)
 
 -- | Start a timer.
 startTimer :: (MonadIO m, Num a) => Int -> (a -> IO ()) -> m Timer
 startTimer delay action = liftIO $ do
-  t <- newEmptyMVar
-  _ <- forkIO $ go t 1
-  return (Timer t)
+  m <- newMVar True
+  _ <- forkIO $ go m 1
+  return (Timer m)
   where
-    -- loop until something is put into the MVar
-    go t count = do
+    -- run until False is put into the MVar
+    go m count = do
       threadDelay delay
-      isRunning <- isEmptyMVar t
+      isRunning <- takeMVar m
       when (isRunning) $ do
         action count
-        go t (count + 1)
+        putMVar m isRunning
+        go m (count + 1)
 
--- | Stop given timer.
+-- | Stop timer; block until the timer is stopped.
 stopTimer :: (MonadIO m) => Timer -> m ()
-stopTimer (Timer t) = liftIO (putMVar t ())
-
+stopTimer (Timer m) = liftIO (takeMVar m >> putMVar m False)
