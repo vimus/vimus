bd1157fad0fcdb28c5f08405a804ccdce63ef6b5
nand <nand@lavabit.com>
2012-04-26 11:25:46 +0200

    Changed “git co” to “git checkout”

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-git push origin master && git co release && git rebase master && git push origin release -f && git co master
+git push origin master && git checkout release && git rebase master && git push origin release -f && git checkout master
