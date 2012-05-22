93e53f9ed6ecf067f2f1b08472caca93a9b636f5
Simon Hengel <sol@typeful.net>
2012-05-22 08:53:42 +0200

    Fix warning

--- test/QueueSpec.hs
+++ test/QueueSpec.hs
@@ -13,11 +13,11 @@ spec = do
   describe "takeAllQueue" $ do
     it "returns all elements in order" $ do
       q <- newQueue 
-      putQueue q 10 >> putQueue q 20 >> putQueue q 30
+      putQueue q 10 >> putQueue q 20 >> putQueue q (30 :: Int)
       takeAllQueue q `shouldReturn` [10, 20, 30]
 
     it "leaves the queue empty" $ do
       q <- newQueue 
-      putQueue q 10 >> putQueue q 20 >> putQueue q 30
+      putQueue q 10 >> putQueue q 20 >> putQueue q (30 :: Int)
       _ <- takeAllQueue q
       takeAllQueue q `shouldReturn` []
