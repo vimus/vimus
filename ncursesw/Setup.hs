905f887447e8afca6d9cd59992fa005cdfc6a163
Joachim Fasting <joachifm@fastmail.fm>
2014-03-22 08:52:38 +0100

    ncursesw: no probing for ncurses install in Setup
    Figuring out where ncurses is actually installed
    is a task best left to package vendors.
    This patch removes all probe logic from Setup.hs.
    
    We assume that ncurses is installed at
    /usr/include/curses.h.
    Non-standard installs are easily accommodated by
    setting --extra-lib-dirs and --extra-include-dirs.
    For example, Debian installs the ncurses headers
    (libncursesw5-dev) at /usr/include/ncursesw.
    To build ncursesw on Debian, use
    
        cabal install --extra-include-dirs=/usr/include/ncursesw

--- ncursesw/Setup.hs
+++ ncursesw/Setup.hs
@@ -1,28 +1,4 @@
-import Distribution.Simple
-import Distribution.Simple.Setup
-import Distribution.PackageDescription
-import Distribution.Simple.LocalBuildInfo
-
-import System.Directory
+import           Distribution.Simple
 
 main :: IO ()
-main = defaultMainWithHooks simpleUserHooks {
-    preBuild = \_ _ -> mkHookedBuildInfo
-  , preConf  = \_ _ -> mkHookedBuildInfo
-  , postConf = postConfHook
-  }
-  where
-    postConfHook :: Args -> ConfigFlags -> PackageDescription -> LocalBuildInfo -> IO ()
-    postConfHook args flags pd lbi = do
-      bi <- mkHookedBuildInfo
-      postConf simpleUserHooks args flags (updatePackageDescription bi pd)  lbi
-
-    mkHookedBuildInfo :: IO HookedBuildInfo
-    mkHookedBuildInfo = do
-      r <- doesFileExist "/usr/include/ncursesw/ncurses.h"
-      let bi =
-            if r then
-              emptyBuildInfo {cppOptions = ["-DDEBIAN"], ccOptions = ["-DDEBIAN"], includes = ["/usr/include/ncursesw/ncurses.h"]}
-            else
-              emptyBuildInfo {includes = ["/usr/include/ncurses.h"]}
-      return (Just bi, [])
+main = defaultMain
